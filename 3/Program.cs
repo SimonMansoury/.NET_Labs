6970d26fe5cc2dd13c4256ba7e0a07de44362065
olhanikolenko <dragonheart1512@gmail.com>
2020-02-26 22:42:00 +0200

    Add third lab

---
+++ 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CS_Lab_3
+{
+    class Program
+    {
+        /*
+           7)	Розробити систему Поштове відділення. 
+           З видавництва в поштове відділення надходять газети і журнали. 
+           відділення відправляє отримані друковані видання відповідним передплатникам.
+         */
+        static void Main(string[] args)
+        {
+            ConcretePostOffice PostOffice = new ConcretePostOffice();
+
+            ConcreteSubscriber subscriber = new ConcreteSubscriber("Anatoliy");
+            PostOffice.AddSubscriber(subscriber);
+
+            ConcreteSubscriber subscriber1 = new ConcreteSubscriber("Ivanna");
+            PostOffice.AddSubscriber(subscriber1);
+
+            ConcreteSubscriber subscriber2 = new ConcreteSubscriber("Vasiliy");
+            PostOffice.AddSubscriber(subscriber2);
+
+            Random rand = new Random();
+            try
+            {
+                PostOffice.NotifySubscribers(rand.Next(1, 100));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3/Subscriber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Lab_3
+{
+	interface IPostable
+	{
+		void AddSubscriber(ConcreteSubscriber o);
+		void RemoveSubscriber(ConcreteSubscriber ob);
+		void NotifySubscribers(int NumOfIncomingBooks);
+	}
+
+	class ConcretePostOffice : IPostable
+	{
+		List<ConcreteSubscriber> subscribers;
+		
+		public ConcretePostOffice()
+		{
+			subscribers = new List<ConcreteSubscriber>();
+		}
+		public void AddSubscriber(ConcreteSubscriber ob)
+		{
+			subscribers.Add(ob);
+		}
+
+		public void RemoveSubscriber(ConcreteSubscriber ob)
+		{
+			subscribers.Remove(ob);
+		}
+
+		public void NotifySubscribers(int NumOfIncomingBooks)
+		{
+			if (subscribers.Count == 0)
+				throw new Exception("There`s no subscribers!");
+
+			foreach(var sub in subscribers)
+				Console.WriteLine($"Subscriber {sub.SubscriberName} gets a message, that we have {NumOfIncomingBooks} new Books!");
+		}
+
+	}
+
+	interface ISubscriber
+	{
+		public void Update(object o);
+	}
+	class ConcreteSubscriber : ISubscriber
+	{
+		public ConcreteSubscriber() : this("No Name") { }
+		public ConcreteSubscriber(string Name)
+		{
+			SubscriberName = Name;
+		}
+		public string SubscriberName { get; private set; }
+
+		public void Update(object o)
+		{
+
+		}
+	}
+}
